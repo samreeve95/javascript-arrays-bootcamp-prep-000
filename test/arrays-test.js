eec3eb7d69ec7d5c64049e35b1afebb706b7fd4c
Sam Reeve <sam.reeve95@gmail.com>
2019-04-26 04:05:01 +0000

    Automatically backed up by Learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -32,13 +32,13 @@ describe('arrays', () => {
 
   describe('destructivelyAddElementToBeginningOfArray(array, element)', () => {
     it('adds an element to the beginning of an array', () => {
-      expect(destructivelyAddElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
+      expect(destructivelyAddElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1]);
     })
 
     it('alters the original array', () => {
       const array = [1]
 
-      destructivelyAddElementToBeginningOfArray(array, 'foo')
+      destructivelyAddElementToBeginningOfArray(array, 'foo');
 
       expect(array).to.eql(['foo', 1]);
     })
