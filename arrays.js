b88dbff9ab6d90df9af32a6dae69b36edd298381
Sam Reeve <sam.reeve95@gmail.com>
2019-04-26 04:39:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -36,6 +36,7 @@ function accessElementInArray(array, index)
 function destructivelyRemoveElementFromBeginningOfArray(array)
 {
   array.shift();
+  return array;
 }
 function removeElementFromBeginningOfArray(array)
 {
@@ -48,6 +49,7 @@ function removeElementFromBeginningOfArray(array)
 function destructivelyRemoveElementFromEndOfArray(array)
 {
   array.pop();
+  return array;
 }
 function removeElementFromEndOfArray(array)
 {
