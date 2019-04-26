3c81a1ff0e3e3bf58071c6b43f19163d251d2d5d
Sam Reeve <sam.reeve95@gmail.com>
2019-04-26 04:33:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,8 @@ function addElementToEndOfArray(array, element)
 
 function destructivelyAddElementToEndOfArray(array, element)
 {
-  
+  array.push(element);
+  return array;
 }
 function accessElementInArray(array, index)
 {
