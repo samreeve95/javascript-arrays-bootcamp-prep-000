f87a23c29fb0f9910ab8ae06d74dae04d7928f6a
Sam Reeve <sam.reeve95@gmail.com>
2019-04-26 04:04:58 +0000

    Automatically backed up by Learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -40,47 +40,47 @@ describe('arrays', () => {
 
       destructivelyAddElementToBeginningOfArray(array, 'foo')
 
-      expect(array).to.eql(['foo', 1])
+      expect(array).to.eql(['foo', 1]);
     })
   })
 
   describe('addElementToEndOfArray(array, element)', () => {
     it('adds an element to the end of an array', () => {
-      expect(addElementToEndOfArray([1], 'foo')).to.eql([1, 'foo'])
+      expect(addElementToEndOfArray([1], 'foo')).to.eql([1, 'foo']);
     })
 
     it('does not alter the original array', () => {
-      const array = [1]
+      const array = [1]''
 
-      addElementToEndOfArray(array, 'foo')
+      addElementToEndOfArray(array, 'foo');
 
-      expect(array).to.eql([1])
+      expect(array).to.eql([1]);
     })
   })
 
   describe('destructivelyAddElementToEndOfArray(array, element)', () => {
     it('adds an element to the end of an array', () => {
-      expect(destructivelyAddElementToEndOfArray([1], 'foo')).to.eql([1, 'foo'])
+      expect(destructivelyAddElementToEndOfArray([1], 'foo')).to.eql([1, 'foo']);
     })
 
     it('alters the original array', () => {
       const array = [1]
 
-      destructivelyAddElementToEndOfArray(array, 'foo')
+      destructivelyAddElementToEndOfArray(array, 'foo');
 
-      expect(array).to.eql([1, 'foo'])
+      expect(array).to.eql([1, 'foo']);
     })
   })
 
   describe('accessElementInArray(array, index)', () => {
     it('accesses the element in array at the given index', () => {
-      expect(accessElementInArray([1, 2, 3], 2)).to.equal(3)
+      expect(accessElementInArray([1, 2, 3], 2)).to.equal(3);
     })
   })
   
   describe('destructivelyRemoveElementFromBeginningOfArray(array)', ()=>{
     it('returns the array with the first element removed', () => {
-      expect(destructivelyRemoveElementFromBeginningOfArray([1, 2, 3])).to.eql([2, 3])
+      expect(destructivelyRemoveElementFromBeginningOfArray([1, 2, 3])).to.eql([2, 3]);
     })
     
     it('alters the original array', ()=>{
