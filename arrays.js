52efd053a69e28c8f343f4d69dca51edbdbe1749
Sam Reeve <sam.reeve95@gmail.com>
2019-04-26 04:20:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,8 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element)
 {
-  array.unshift(element);
+  var ar = array;
+  
 }
 
 var array=[1];
